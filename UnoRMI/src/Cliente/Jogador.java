76e271f598831aef2b5ef7265ba7a8d27f4bff4d
gabrielNetto94 <silveiranetto94@gmail.com>
2019-07-03 21:50:27 -0300

    alterando para RMI o projeto

---
+++ UnoRMI/src/Cliente/Jogador.java
@@ -0,0 +1,14 @@
+package Cliente;
+
+
+import java.util.LinkedList;
+
+
+public class Jogador {
+    
+    int numero;
+    //por enquanto não está sendo usado
+    LinkedList<Carta> cartasMao = new LinkedList<>();
+    
+    
+}
---
+++ UnoRMI/src/Servidor/Carta.java
@@ -0,0 +1,13 @@
+package Servidor;
+
+public class Carta {
+
+    public int numero;
+    public String cor;
+    
+    public Carta(String cor, int numero){
+        this.cor = cor;
+        this.numero = numero;
+    }
+    
+}
